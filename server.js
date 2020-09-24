18641e878845f8484ea2085d5d91df63c1252f68
Georgios Mpovolis <gam154@scarletmail.rutgers.edu>
2020-09-24 18:46:40 -0400

    create the basis of the server running on express just missing the routes that are yet to be created

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const handle = require("express-handlebars");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", handle({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// require routes and use them
+// const routes = require("")
+// app.use(routes)
+
+app.listen(PORT, function(){
+    console.log(`Listening on: http://localhost:${PORT}`);
+});
