df4d91ae6f46d97cc2e523e97ece0f31b7013746
Georgios Mpovolis <gam154@scarletmail.rutgers.edu>
2020-09-29 09:30:30 -0400

    added public style sheet and js functionality

---
+++ public/assets/js/index.js
@@ -0,0 +1,64 @@
+$(function() {
+  $(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevour");
+
+    var newDevourState = {
+      devour: newDevour
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("changed to devoured", newDevour);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#bu").val().trim(),
+      devour: $("[burger_name=devour]:checked").val()
+    };
+
+    // Send the POST request.
+    if(newBurger.length !== 0){
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created a new burger!");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+    } else {
+      alert("Please enter a burger type!");
+    }
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted a burger!", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
