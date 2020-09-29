1eb54bdec6b24c53a42bc69352a0756f02d37276
Georgios Mpovolis <gam154@scarletmail.rutgers.edu>
2020-09-29 09:29:32 -0400

    added files to allow connection to mysql

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+require("dotenv").config();
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        PORT: 3000,
+        user: "root",
+        password: "blu3snak3",
+        database: "burgers_db"
+    });
+};
+
+connection.connect(function(err){
+    if(err){
+        console.log("error", err)
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
